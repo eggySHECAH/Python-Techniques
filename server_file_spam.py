2b82b16ff4229a1bc83b6ebdc0b5992d39a0d4f4
James Marke ⠕ <72310196+eggySHECAH@users.noreply.github.com>
2023-01-24 14:20:23 +0000

    Server Spammer

--- server_file_spam.py
+++ server_file_spam.py
@@ -1,1 +1,17 @@
 # A file spammer
+class Filegen:
+  def __init__(self, file: int):
+    self.text = file
+    self.file = open(self.text, 'w')
+    self.file.write("Hello!")
+
+letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','ә', 'б', 'в', 'г', 'ғ', 'д', 'ё', 'ж', 'з', 'и', 'й', 'к', 'қ', 'л', 'м', 'н']
+for letter in letters:
+    try:
+      text = letter
+      while True:
+        fg = Filegen(text)
+        text += letter
+        print(letter)
+    except:
+      continue
