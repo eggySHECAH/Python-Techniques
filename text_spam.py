46db738047fb76cbcf7dfe22b4555d62998024f5
James Marke ⠕ <72310196+eggySHECAH@users.noreply.github.com>
2023-01-24 14:17:03 +0000

    text spam

--- text_spam.py
+++ text_spam.py
@@ -1,1 +1,10 @@
 # spams text onto a specific file
+class Spam:
+    def __init__(self, file: str, text: str):
+        self.f = file
+        self.t = text
+        self.file = open(self.f, 'w')
+        while True:
+            self.file.write(self.t)
+
+s = Spam('demo_file.rtf', 'Lorem ipsum sit dolor amet... ')
